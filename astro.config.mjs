db90e960749430100f6ae6164987b968ea0732dc
vlsingletonconsulting <vsingletonconsulting@gmail.com>
2026-03-02 18:15:38 -0500

    Fix: remove sitemap to resolve build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://portfolio.vsingleton.com',
   output: 'static',
   compressHTML: true,
-  integrations: [
-    sitemap(),
-  ],
 });
